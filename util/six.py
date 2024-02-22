b3bdafd1773959a66c261794522e65d75b839944
goose-poet <Li_bw0902@163.com>
2024-02-22 11:21:32 +0800

    取消了对PY2的支持

--- util/six.py
+++ util/six.py
@@ -20,24 +20,18 @@ PY3 = sys.version_info[0] == 3
 if PY3:
     def iteritems(d, **kw):
         return iter(d.items(**kw))
-else:
-    def iteritems(d, **kw):
-        return d.iteritems(**kw)
+
 
 if PY3:
     from urllib.parse import urlparse
-else:
-    from urlparse import urlparse
+
 
 if PY3:
     from imp import reload as reload_six
-else:
-    reload_six = reload
+
 
 if PY3:
     from queue import Empty, Queue
-else:
-    from Queue import Empty, Queue
 
 
 def withMetaclass(meta, *bases):
